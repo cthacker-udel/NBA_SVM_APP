ed68f8291a43e330caa9da03b592a0a058555599
cthacker-udel <cthacker@udel.edu>
2023-01-19 16:17:25 -0500

    [master] Added layout and home page

--- src/common/Layout/Layout.component.tsx
+++ src/common/Layout/Layout.component.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any -- disabled */
 import React, { type ReactNode } from "react";
 
 import styles from "./Layout.module.css";
 
 type LayoutProperties = {
+    backgroundImage?: any;
     children: ReactNode;
 };
 
@@ -10,9 +12,18 @@ type LayoutProperties = {
  * The common layout component, which allows for all children to be wrapped properly
  *
  * @param props - The properties of the component
+ * @param props.backgroundImage - The background image to apply to the layout
  * @param props.children - The ReactNode this HOC component "wraps"
  * @returns The layout applied to the child
  */
-export const Layout = ({ children }: LayoutProperties): JSX.Element => (
-    <div className={styles.layout_container}>{children}</div>
+export const Layout = ({
+    backgroundImage,
+    children,
+}: LayoutProperties): JSX.Element => (
+    <div
+        className={styles.layout_container}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+        {children}
+    </div>
 );
--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
+import analyticsGif from "src/assets/backgrounds/homepage.gif";
 import spinningBasketball from "src/assets/gifs/spinning_basketball.gif";
 
 import { Layout } from "../../common";
@@ -11,7 +12,7 @@ import styles from "./Home.module.css";
  * @returns Home page
  */
 export const Home = (): JSX.Element => (
-    <Layout>
+    <Layout backgroundImage={analyticsGif}>
         <div className={styles.home_page_content}>
             <div className={styles.home_page_title}>
                 <Image
@@ -28,6 +29,11 @@ export const Home = (): JSX.Element => (
                 {
                     "This is an application for testing my knowledge of the ML topics I have been learning so far. We will be using the SVM model to predict a target, given proper prediction data. The backend will be entirely in python for ease of backend use, and will be fetching from nba api to pull player data as well. It will have a fully functional front-end, and pulling live data to populate the GUI."
                 }
+            </div>
+            <div className={styles.home_page_button_bar}>
+                <Button variant="secondary">
+                    {"Enter the NBA Tracker App!"}
+                </Button>
             </div>
         </div>
     </Layout>
